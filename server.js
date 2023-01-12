dd50f9e6a1901f3f49ab12e1d451957bc5682555
khang <khang.hoang@thlone.vn>
2023-01-12 13:13:10 +0700

    connect mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config()
 const helmet = require('helmet'); // using for security header
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const userRoute = require('./Routes/User.route')
 
@@ -10,6 +11,37 @@ const PORT = process.env.PORT || 5000;
 
 app.use(helmet())
 app.use(morgan('common'))
+
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://localhost:27017/users', { serverSelectionTimeoutMS: 3000 })
+    .then(result => {
+        console.log("Database connect success")
+    })
+    .catch(err => {
+        console.log("Database connect fail!", err)
+    })
+
+mongoose.connection.on('connected', () => {
+    console.log("Mongodb connected to db!")
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(err.message);
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongodb connected to db!');
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongodb connected is disconnected!');
+})
+
+process.on("SIGINT", async () => {
+    await mongoose.connection.close();
+    process.exit(0);
+})
+
 app.use('/v1', userRoute)
 
 app.get("/", (req, res, next) => {
